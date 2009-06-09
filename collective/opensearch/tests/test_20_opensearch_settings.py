b59fc611e132c5376f6cf4c10564da528f5a6695
pumazi <pumazi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-09 14:08:12 +0000

    Modified the import procedure to use the runImportStepFromProfile rather than  manage_importSelectedSteps, because _mangleTimestampName, which is called from within manage_importSelectedSteps, can't handle the millisecond operation.

--- collective/opensearch/tests/test_20_opensearch_settings.py
+++ collective/opensearch/tests/test_20_opensearch_settings.py
@@ -22,7 +22,7 @@ class TestOpenSearchAdapter(PloneTestCase.PloneTestCase):
         portal_setup = self.portal.portal_setup
         # Import the propertiestool step and its dependencies from the default profile.
         self.loginAsPortalOwner()
-        portal_setup.manage_importSelectedSteps(['propertiestool'], True, 'profile-collective.opensearch:default')
+        portal_setup.runImportStepFromProfile('profile-collective.opensearch:default', 'propertiestool')
         self.logout()
 
     def test_verify_install(self):
@@ -88,7 +88,7 @@ class TestOpenSearchAdapter(PloneTestCase.PloneTestCase):
         portal_setup = self.portal.portal_setup
         # Import the propertiestool step and its dependencies from the uninstall profile.
         self.loginAsPortalOwner()
-        portal_setup.manage_importSelectedSteps(['propertiestool'], True, 'profile-collective.opensearch:uninstall')
+        portal_setup.runImportStepFromProfile('profile-collective.opensearch:uninstall', 'propertiestool')
         self.logout()
         # Verify the propertiestool step deleted the opensearch_properties object.
         portal_properties = self.portal.portal_properties
