ada8811ea0781e5f2ff74bb597439341dd0af987
pumazi <pumazi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-12 14:18:06 +0000

    Made the uninstall setuphandler check if the object exists before attempting to remove it.

--- collective/opensearch/setuphandlers.py
+++ collective/opensearch/setuphandlers.py
@@ -16,10 +16,10 @@ def uninstallPropertiesTool(site):
     if site.readDataFile('collective.opensearch-propertiestool_uninstall.txt') is None:
         return
     portal_properties = getToolByName(site.getSite(),'portal_properties')
-    del portal_properties['opensearch_properties']
+    if portal_properties.get('opensearch_properties', None):
+        del portal_properties['opensearch_properties']
 
 
 # def uninstallActionIcons(site):
 #     if site.readDataFile('collective.opensearch-actionicons_uninstall.txt') is None:
 #         return
-#     portal_properties = getToolByName(site.getSite(),'portal_actions')
