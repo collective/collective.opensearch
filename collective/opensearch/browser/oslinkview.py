390bb61e82ba6794635b01fb86b123e4ae02b502
nan010 <nan010@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-29 08:30:11 +0000

    display html if a (x)html description is returned in the atom/rss feed

--- collective/opensearch/browser/oslinkview.py
+++ collective/opensearch/browser/oslinkview.py
@@ -1,3 +1,19 @@
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+import logging
 from zope.interface import implements, Interface
 
 from Products.Five import BrowserView
@@ -6,6 +22,8 @@ from Products.CMFCore.utils import getToolByName
 from collective.opensearch import opensearchMessageFactory as _
 import feedparser
 
+
+logger = logging.getLogger('collective.opensearch')
 
 class IOsLinkView(Interface):
     """
@@ -27,6 +45,17 @@ class OsLinkView(BrowserView):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+
+    def is_html(self, entry):
+        if entry['summary_detail']['type'] in ['text/html',
+                                            'application/xhtml+xml']:
+            return True
+        elif entry['summary_detail']['type']=='text/plain':
+            return False
+        else:
+            logger.info('Unknown format for summary detail: %s' %
+                entry['summary_detail']['type'])
+            return False
 
     def get_search_results(self):
         url = self.context.getRemoteUrl()
